fc1ceac716c44064495f1d604e34f660f6c57366
Luara Raggio <lraggio@MacBook-Air-de-Luara.local>
2024-03-28 20:14:15 -0300

    needs norminette

---
+++ client.c
@@ -0,0 +1,38 @@
+#include "libft/libft.h"
+#include <signal.h>
+
+void	send_char_bit_by_bit(int pid, char c)
+{
+	int	i;
+	
+	i = 8;
+	while (i--)
+	{
+		if (((c >> i) & 1) == 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(200);
+	}
+}
+
+int     main(int argc, char **argv)
+{
+        int     i;
+        int     pid;
+
+        i = 0;
+        if (argc != 3)
+        {
+		ft_printf("Invalid format.\n");
+                ft_printf ("It must be: ./server + 'server_pid' + message");
+		return (0);
+	}
+	pid = ft_atoi(argv[1]);
+	while(argv[2][i])
+	{
+		send_char_bit_by_bit(pid, argv[2][i]);
+		i++;
+	}
+	send_char_bit_by_bit(pid, '\n');
+}
